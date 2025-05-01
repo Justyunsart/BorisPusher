1f6d9bbe74dea1e64abf574fcac6a1ee10adb127
justyunsart <yunroh02@gmail.com>
2025-05-01 16:29:11 -0700

    removed commented code on BorisGui.py

--- Scripts/BorisGui.py
+++ Scripts/BorisGui.py
@@ -6,18 +6,16 @@ from CurrentGuiClasses import *
 from BorisGuiClasses import *
 import palettes as p
 from ScrollableFrame import ScrollableFrame
-#######
-# GUI #
-#######
+
+"""
+This script is directly called when running main.py - the function OpenGUI() defined below assembles the tkinter GUI and runs its mainloop.
+"""
 def OpenGUI():
-    #print(f"OpenGUI called.")
     '''
     Testing the code by changing parameters and numsteps and everything became too cumbersone, so I decided to
     bite the bullet and create some GUI for it. 
-
     This is the main hub for the GUI, with many helper files providing classes and functions.
     '''
-
     # Application Window
     root = tk.Tk()
     root.title("Configure Sim")
@@ -33,12 +31,11 @@ def OpenGUI():
     #======#
     # MENU #
     #======#
-
     # add the toolbar
     mainToolbar = ConfigMenuBar(Main)
 
     """
-    KEY VARS
+    DIRECTORY VARIABLES
     """
     # Selected dirs for sims
     Main_PrefFiles = Main.prefs
@@ -48,11 +45,10 @@ def OpenGUI():
     DIR_lastUsed = Main_PrefFiles.FILE_lastUsed
     DIR_Output = Main_PrefFiles.DIR_output
 
-    #print(DIR_Particle)
-
     # PLOT OR CALCULATE?
     """
     MAIN TABS
+    The 'plot' or 'calculate' tab on the top of the GUI window.
     """
     style1 = ttk.Style()
     style1.theme_use('default')
@@ -75,28 +71,16 @@ def OpenGUI():
         font=[("selected", ('Arial', 18, 'bold'))]
     )
 
-
     tabControl=ttk.Notebook(Main, padding="0 12 2 0", style='Two.TNotebook')
-    #tabControl.grid(row=0, column=0, sticky=N+W+E+S)
-
     ## Create tab objects
     tab_calc = tk.Frame(tabControl, bg='gray')
-
-    #tab_calc.grid_columnconfigure(0, weight = 1)
-    #tab_calc.grid_rowconfigure(0, weight = 1)
-
     tab_plot = ttk.Frame(tabControl, padding="4 12 12 4")
-    #tab_plot.grid_columnconfigure(0, weight = 1)
-    #tab_plot.grid_rowconfigure(0, weight = 1)
-
-
-    #tabControl.grid_rowconfigure(0, weight=1)
-    #tabControl.grid_columnconfigure(0, weight=1)
 
 
     '''
     NESTED TABS:
     Here I will define a style for this, as I want these tabs to look different from the first ones.
+    These tabs are for the coil and particle parameter options in the main calculate tab.
     '''
 
     style = ttk.Style()
@@ -127,18 +111,8 @@ def OpenGUI():
     calc_frame1 = tk.Frame(calc_nested_notebook, relief='flat', background="light gray")
     calc_frame1_scroll = ScrollableFrame(calc_frame1)
 
-    # Scroll wheel for tabs
-    #calc_frame1_scrollwheel = tk.Scrollbar(calc_frame1, orient="vertical")
-    #calc_frame1_scrollwheel.grid(row=0, column=9, sticky="NE")
-    #calc_frame1.grid_columnconfigure(9, weight=1)
-
-    #calc_frame2 = ttk.Frame(calc_nested_notebook, width=200, height=100, relief=tk.SUNKEN)
     calc_frame3 = tk.Frame(calc_nested_notebook, background="light gray")
-
     calc_frame3_scroll = ScrollableFrame(calc_frame3)
-
-    #calc_nested_notebook.grid(row=0, column=0, sticky="nw")
-    #calc_nexted_label_frame = tk.Frame(tab_calc)
 
     # PLOT GUI #
     #=============#
@@ -160,7 +134,7 @@ def OpenGUI():
     ## WIDGETS
     ### Output Files
     label_out_file = ttk.Label(plot_out_file,
-                                text = "Output File Dir:")#.grid(column = 0, row = 1, sticky=(W))
+                                text = "Output File Dir:")
     label_out_file.grid(row = 0, column= 0, padx=10, pady=10)
 
     button_out_file = tk.Button(plot_out_file,
@@ -175,55 +149,29 @@ def OpenGUI():
     #################
     # GRAPH WINDOWS #
     #################
-    #from PlottingWindow import TrajGraph
     from PlottingWindow import PlottingWindowObj
-    #from PlottingWindow_Dash import DashApp_Frame
 
-    #trajectoryGraph = TrajGraph(plot_graph_traj)
-    #trajectoryGraph = DashApp_Frame(plot_graph_traj)
     trajectoryGraph = PlottingWindowObj(plot_graph_traj, Main, name_out_file_var)
     trajectoryGraph.grid(row=0, column=0)
 
-
-    #button_out_file.config(command=partial(browseFiles, name_out_file))
-
-    ### Confirm Button
     '''
     We need to keep watch on the selected data file value, and ensure that this button is active only when
     there is a valid data file selected.
     '''
-    #plot_confirm = tk.Button(tab_plot,
-    #                        text="Create Plot",
-    #                        state="disabled")
     button_out_file.config(command=partial(PlotFileCallback, name_out_file_var, DIR_Output))
-    #plot_confirm.config(command=partial(trajectoryGraph.UpdateGraph, name_out_file))
-    #plot_confirm.config(command=partial(trajectoryGraph.loadApp, name_out_file))
-
-
-    #plot_confirm.grid(sticky=S)
-
-
-
-    # CALCULATION GUI#
 
     #=============#
     # GUI WIDGETS #
     #=============#
-
     style = ttk.Style()
     style.configure("LG.TCheckbutton", background="light gray", foreground="black", indicatorbackground="black", indicatorforeground="white")
 
     # FRAMES
-
-
     ## MAIN CONTAINER
     calc_title_LFrame = ttk.Frame(calc_frame1_scroll.frame)
     calc_title_LFrame.columnconfigure(0, weight=1)
-    #calc_title_LFrame.grid(row=0, column=0, pady=10, padx=10, sticky="NW")
 
     ### SUB CONTAINERS
-
-
     CalcCheckBoxFrame = tk.Frame(calc_title_LFrame, bg="light gray")
     CalcCheckBoxFrame.grid(row=1, column=0, pady=10, padx=10, sticky="WSEW")
 
@@ -231,7 +179,6 @@ def OpenGUI():
     CalcRestartFileFrame.grid(row=2, column= 0, pady=10, padx=10)
 
     Particle = tk.LabelFrame(calc_frame1_scroll.frame, text="Particle Conditions")
-    #Particle.grid(row=1, column=0, sticky="E")
 
     DropdownFrame = tk.Frame(Particle)
     DropdownFrame.grid(row=0, column=0, sticky="WSEW")
@@ -244,7 +191,6 @@ def OpenGUI():
 
 
     FieldGraphs = tk.LabelFrame(calc_frame1_scroll.frame, bg="light gray", text="Fields")
-    #FieldGraphs.grid(row=0, column=1)
 
     Fields = tk.LabelFrame(FieldGraphs, bg="light gray", text="Static Fields")
     Fields.grid(row=0, column=0, sticky="WSEW", padx=10, pady=10)
@@ -253,8 +199,6 @@ def OpenGUI():
     Fields0.grid(row=0, column=0, sticky="WSEW", padx=5, pady=5)
     Fields1 = tk.Frame(Fields)
     Fields1.grid(row=0, column=1, sticky="WSEW", padx=5, pady=5)
-    #BGraphFrame = tk.LabelFrame(FieldGraphs, bg="light gray", text="B-field")
-    #BGraphFrame.grid(row=0, column=0)
 
     EGraphFrame = tk.LabelFrame(FieldGraphs, bg="light gray", text="E-field")
     EGraphFrame.grid(row=1, column=0)
@@ -269,20 +213,18 @@ def OpenGUI():
     Combobox_particle_file = FileDropdown(DropdownFrame,
                                                     dir=DIR_Particle)
     Combobox_particle_file.grid(row=0, column=0)
-    #particleCheckboxes = ParticlePreviewSettings(DropdownFrame)
     particlePreview = ParticlePreview(ParticlePreviewFrame,
                                     Combobox_particle_file)
     calc_frame1_scroll._add_Subscriber(particlePreview) # I do this so that I can run the table's update function when this tab becomes selected.
+    
     ## FIELDS!!!!!!
     import FieldMethods as fm
-    #b_field = CoordTable(Fields, title="B-Field")
     b_field = FieldDropdown(Fields0,
                         fm.B_Methods,
                         "B-Field: ",
                         default=1)
     b_field.grid(row=0, column=0)
 
-    #e_field = E_CoordTable(Fields, title="E-Field")
     e_field= FieldDropdown(Fields0,
                         fm.E_Methods,
                         "E-Field: ")
@@ -299,20 +241,6 @@ def OpenGUI():
                             height=2,
                             font=("None", 12))
     calc_button.pack(anchor='s', side=BOTTOM)
-
-    '''
-    calc_nexted_label_frame.pack(side=TOP, anchor= NW, fill=Y)
-    submenu = tk.Label(calc_nexted_label_frame, text="Submenus", 
-                    foreground='white', 
-                    padx=15, pady=5,
-                    background=palette.Background.value, 
-                    width=10,
-                    font=("Arial", 12, "underline"),
-                    justify="center"
-                    ).pack(side=LEFT)
-    '''
-
-
 
     #===========================================================================================#
     ########################
@@ -344,30 +272,6 @@ def OpenGUI():
     ## Timestep stuff..
     time_info = TimeStep_n_NumStep(CalcTimeStepFrame, coil_config.table)
 
-    """
-    '''
-    #the place to see and change the configuration of the magnetic coils.
-    #Will happen in a new window because why not
-    '''
-    root.update() # set info for positioning with other windows
-    toplevel = tk.Toplevel(root)
-
-    # toplevel positioning, based on the dimension and offset of the root window.
-    toplevel_offsetx, toplevel_offsety = root.winfo_x() + root.winfo_width(), root.winfo_y()
-    toplevel.geometry(f"+{toplevel_offsetx}+{toplevel_offsety}")
-
-    CurrentFrame = tk.LabelFrame(toplevel, text="Configure Current")
-    CurrentFrame.grid(row = 0, padx=10, pady=10)
-
-    #CurrentFile = tk.Frame(CurrentFrame)
-    #CurrentFile.grid(row=0, column=0)
-    #CurrentTable = tk.Frame(CurrentFrame)
-    #CurrentTable.grid(row=1, column=0)
-
-    #coil_file = Particle_File_Dropdown(CurrentFile, DIR_Coil)
-    #coil_table = CurrentEntryTable(CurrentTable, CircleCurrentConfig, coil_file)
-    coil_config = CurrentConfig(CurrentFrame, DIR_Coil, DIR_coilDefs)
-    """
     #=======#
     # FINAL #
     #=======#
@@ -389,7 +293,6 @@ def OpenGUI():
 
 
     calc_nested_notebook.add(calc_frame1, text="Particle")
-    #calc_nested_notebook.add(calc_frame2, text="Particle")
 
     calc_nested_notebook.add(calc_frame3, text="Coil")
     calc_nested_notebook.pack(expand=True, fill='both', side=LEFT)
