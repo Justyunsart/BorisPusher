8e109051f9882ab4407a2491577c06085bb78b57
Frank Wessel <fwessel@gmail.com>
2025-07-17 15:58:06 -0700

    Rotating vectors to all corners

--- Scripts/EFieldFJW/cubeCorners.py
+++ Scripts/EFieldFJW/cubeCorners.py
@@ -38,28 +38,51 @@ thetaZdeg = np.degrees(thetaY)
 
 # Apply rotations
 v1 = Rx(thetaX) @ v0              # After rotation about x-axis
-v1mag = np.linalg.norm(v1)              # After rotation about x-axis
+v1mag = np.linalg.norm(v1)        # After rotation about x-axis
 v2 = Ry(thetaY) @ v1              # After rotation about y-axis
-v2mag = np.linalg.norm(v2)              # After rotation about x-axis
-# print(v1, v1mag, v2, v2mag)
+v2mag = np.linalg.norm(v2)        # After rotation about x-axis
 v3 = Rz(thetaZ) @ v2              # After another rotation about y-axis
 v4 = Rz(thetaZ) @ v3              # After another rotation about y-axis
 v5 = Rz(thetaZ) @ v4              # After another rotation about y-axis
 
 flipZ = Rx(np.pi)  # 180° about x-axis
-v6 = flipZ @ v2
-v7 = flipZ @ v3
-v8 = flipZ @ v4
-v9 = flipZ @ v5
+v6 = -1 * v2
+v7 = -1 * v3
+v8 = -1 * v4
+v9 =  -1 * v5
 
+vectors = [v0, v1, v2, v3, v4, v5, v6, v7, v8, v9]
+labels = ['V0 = [0 0 1]',
+    'V1 = Rx(35.26) * V0',
+    'V2 = Ry(45) * V1',
+    'V3 = Rz(90) * V2',
+    'V4 = Rz(180) * V2',
+    'V5 = Rz(270) * V2',
+    'V6 = [-1 -1 -1] * V2',
+    'V7 = [-1 -1 -1]  * V3',
+    'V8 = [-1 -1 -1]  * V4',
+    'V9 = [-1 -1 -1] * V5'
+          ]
+rainbow_colors = [
+    '#404040',  # Dark Grey
+    '#B200FF',  # Violet
+    '#FF0000',  # Red
+    '#FF7F00',  # Orange
+    '#00FF00',  # Green
+    '#0000FF',  # Blue
+    '#FF0000',  # Red
+    '#FF7F00',  # Orange
+    '#00FF00',  # Green
+    '#0000FF'  # Blue
+]
 
 # 3D Visualization
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111, projection='3d')
 origin =[0, 0, 0]
-xaxis = [0.5, 0, 0]
-yaxis = [0, 0.5, 0]
-zaxis = [0, 0, 0.5]
+xaxis = [0.3, 0, 0]
+yaxis = [0, 0.3, 0]
+zaxis = [0, 0, 0.3]
 
 # Axes
 ax.quiver(*origin, *xaxis, linewidth =4, color='k')
@@ -70,27 +93,15 @@ ax.quiver(*origin, *zaxis, linewidth=4, color='k')
 endX = np.array(origin) + np.array(xaxis) + [0.2, 0, 0]
 endY = np.array(origin) + np.array(yaxis) + [0, 0.2, 0]
 endZ = np.array(origin) + np.array(zaxis) + [0, 0, 0.2]
-ax.text(*endX, '  X', color='red', fontsize=20, ha='center', va='center')
-ax.text(*endY,'  Y', color='red', fontsize=20, ha='center', va='center')
-ax.text(*endZ, '  Z', color='red', fontsize=20, ha='center', va='center')
+ax.text(*endX,'X', color='k', fontsize=20, fontweight='bold', ha='center', va='center')
+ax.text(*endY,'Y', color='k', fontsize=20, fontweight='bold', ha='center', va='center')
+ax.text(*endZ,'Z', color='k', fontsize=20, fontweight='bold', ha='center', va='center')
 
-# Vectors
-ax.quiver(*origin, *v0, color='gray', linestyle='dashed', linewidth=2.5, label='V0 = [0 0 1]')
-ax.quiver(*origin, *v1, color='grey', linewidth=2.5, label='Rx(35.26) V0')
-ax.quiver(*origin, *v2, color='m', linewidth=2.5, label = 'V2')
-# 'label=')Ry(45) Rx(35.26) V0')
-ax.quiver(*origin, *v3, color='m', linewidth=2.5, label = 'V3')
-# label='Rz(90) Ry(45) Rx(35.26) V0')
-ax.quiver(*origin, *v4, color='m', linewidth=2.5, label = 'V4')
-# label='Rz(180) Ry(45) Rx(35.26) V0')
-ax.quiver(*origin, *v5, color='m', linewidth=2.5, label = 'V5')
-# label='Rz(270) Ry(45) Rx(35.26) V0')
-ax.quiver(*origin, *v6, color='m', linewidth=2.5, label='Flip V2')
-ax.quiver(*origin, *v7, color='m', linewidth=2.5, label='Flip V3')
-ax.quiver(*origin, *v8, color='m', linewidth=2.5, label='Flip V4')
-ax.quiver(*origin, *v9, color='m', linewidth=2.5, label='Flip V5')
+# Plot Vectors
+for vec, label, color in zip(vectors, labels, rainbow_colors):
+    ax.quiver(*origin, *vec, color = color, linewidth = 2.5, label = label)
 
-# Axes settings
+# Axes
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
 ax.set_zlim([-1, 1])
