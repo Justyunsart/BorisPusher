786994fe0b2098063a5e3eb79418d257ca74a3b2
justyunsart <yunroh02@gmail.com>
2025-09-07 18:38:42 -0700

    working on adding + migrating code to modular, single-responsiblity classes.
    added classes to auto-layout components.
    partly implemented modifier for file widgets.

--- Scripts/Gui_tkinter/widgets/base.py
+++ Scripts/Gui_tkinter/widgets/base.py
@@ -1,6 +1,11 @@
 
 """
-class definitions for base level widgets.
+class definitions for base level widgets
+think of this as modifiers that any class can opt-in to for added functionality.
+
+These classes should:
+    - always focus on doing one thing really well - single responsibility
+    - never / rarely couple with another class, unless there is a good reason
 """
 
 class ParamWidget:
@@ -23,3 +28,41 @@ class ParamWidget:
         for part in parts[:-1]:
             obj = getattr(obj, part)
         return getattr(obj, parts[-1])
+
+class FileWidget:
+    """
+    Widgets that read input files (basically field method, particle widgets)
+
+    There are some common attributes to maintain:
+        - the input dir
+        - acceptable presets (when creating new file)
+    There are common functions too:
+        - save file
+        - load file (read csv)
+        - create new file
+
+    Because the way to track these things can vary by implementation, this class will only be limited
+    to the backend functions. This class should never couple with specific GUI types/instances.
+    """
+    def __init__(self):
+        # Initialize attributes (to be filled another time)
+        self.input_dir = None
+        self.presets = None
+
+    # Value initialization is done MANUALLY because it will be easier to work with in some forms
+    #   - If a widget has lots of these opt-in modifiers, it might get annoying to handle all the args expected.
+    #   - This way, you have time to format inputs outside the __init__ call.
+    # Remember to call this sometime in __init__ though!
+    def fill_file_widget_attrs(self, input_dir, presets):
+        self.input_dir = input_dir
+        self.presets = presets
+
+    # TODO: Move implementation in CurrentEntryTable here
+    def save_file(self):
+        pass
+
+    def load_file(self):
+        pass
+
+    def create_file(self):
+        pass
---
+++ Scripts/settings/fields/FieldSolvers.py
@@ -0,0 +1,9 @@
+
+"""
+Big container class that holds solver + metadata for each field method.
+For example, each method will be associated with different input file directories, or
+have different eligible preset input files for when there are no valid inputs currently existing.
+"""
+
+from EFieldFJW import (e_solvers, e_graphs)
+
